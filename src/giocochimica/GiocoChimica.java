fa52dc28a5fe9e68d24c8269d359744ea46d0786
raffaele.lubrano <raffaele.lubrano@InfoTele23.maxwell.local>
2023-04-17 11:45:33 +0200

    si

--- src/giocochimica/GiocoChimica.java
+++ src/giocochimica/GiocoChimica.java
@@ -1,4 +1,5 @@
 package giocochimica; 
+
 import java.awt.MouseInfo;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -7,20 +8,24 @@ import javax.swing.*;
 public class GiocoChimica extends JFrame {
     
     private JLabel Fuoco;
+    private JLabel Fuoco1;
     private JLabel Acqua;
     private JLabel Terra;
     private JLabel Vento;
+    private JLabel Energia;
     
-    private boolean mousepressed1 = false;
-    private boolean mousereleased1 = true; 
-    private boolean mousepressed2 = false;
-    private boolean mousereleased2 = true;
-    private boolean mousepressed3 = false;
-    private boolean mousereleased3 = true;
-    private boolean mousepressed4 = false;
-    private boolean mousereleased4 = true;
-    
-    
+    private boolean mousepressedFuoco = false;
+    private boolean mousereleasedFuoco = true;
+    private boolean mousepressedFuoco1 = false;
+    private boolean mousereleasedFuoco1 = true;
+    private boolean mousepressedAcqua = false;
+    private boolean mousereleasedAcqua = true;
+    private boolean mousepressedTerra = false;
+    private boolean mousereleasedTerra = true;
+    private boolean mousepressedVento = false;
+    private boolean mousereleasedVento = true;
+    private boolean mousepressedEnergia = false;
+    private boolean mousereleasedEnergia = true;
     
     public GiocoChimica(){
         init();
@@ -38,24 +43,44 @@ public class GiocoChimica extends JFrame {
         
         Fuoco=new JLabel("");
         Fuoco.setSize(200, 200);
-        Fuoco.setLocation(1700,50);
+        Fuoco.setLocation(1675,50);
         Fuoco.setIcon(new ImageIcon("fuocoMoltoBello.png"));
-        Fuoco.addMouseListener(ml1);
+        Fuoco.addMouseListener(mlFuoco);
         this.add(Fuoco);
+        
+        Fuoco1=new JLabel("");
+        Fuoco1.setSize(200, 200);
+        Fuoco1.setLocation(1000,50);
+        Fuoco1.setIcon(new ImageIcon("fuocoMoltoBello.png"));
+        Fuoco1.addMouseListener(mlFuoco1);
+        this.add(Fuoco1);
         
         Acqua=new JLabel("");
         Acqua.setSize(198, 250);
-        Acqua.setLocation(500,300);
+        Acqua.setLocation(1700,250);
         Acqua.setIcon(new ImageIcon("acqua.png"));
-        Acqua.addMouseListener(ml2);
+        Acqua.addMouseListener(mlAcqua);
         this.add(Acqua);
         
+        Terra=new JLabel("");
+        Terra.setSize(200, 200);
+        Terra.setLocation(1700,550);
+        Terra.setIcon(new ImageIcon("Terra.png"));
+        Terra.addMouseListener(mlTerra);
+        this.add(Terra);
         
+        Vento=new JLabel("");
+        Vento.setSize(200, 200);
+        Vento.setLocation(1700,650);
+        Vento.setIcon(new ImageIcon("vento.png"));
+        Vento.addMouseListener(mlVento);
+        this.add(Vento);
         
         
     }
     
     public void loop(){
+        
         int i=0;
         int Mousex = 0;
         int Mousey = 0;
@@ -64,54 +89,74 @@ public class GiocoChimica extends JFrame {
             Mousex = (int) MouseInfo.getPointerInfo().getLocation().getX();
             Mousey = (int) MouseInfo.getPointerInfo().getLocation().getY();
             
-            if(mousepressed1 && !mousereleased1){
-                Fuoco.setLocation(Mousex  - 100 , Mousey  - 73);
+            if(mousepressedFuoco && !mousereleasedFuoco){
+                Fuoco.setLocation(Mousex  - 100 , Mousey  - 150);
             }
-            if(mousepressed2 && !mousereleased2){
-                Acqua.setLocation(Mousex  - 200 , Mousey  - 110); 
+            if(mousepressedFuoco1 && !mousereleasedFuoco1){
+                Fuoco1.setLocation(Mousex  - 100 , Mousey  - 150);
             }
-            if(mousepressed3 && !mousereleased3){
-                Terra.setLocation(Mousex  - 100 , Mousey  - 73);
+            if(mousepressedAcqua && !mousereleasedAcqua){
+                Acqua.setLocation(Mousex -85  , Mousey  -155); 
             }
-            if(mousepressed4 && !mousereleased4){
+            if(mousepressedTerra && !mousereleasedTerra){
+                Terra.setLocation(Mousex  - 100 , Mousey  - 125);
+            }
+            if(mousepressedVento && !mousereleasedVento){
                 Vento.setLocation(Mousex  - 100 , Mousey  - 73);
             }
+            if(mousepressedEnergia && !mousereleasedEnergia){
+                Energia.setLocation(Mousex  - 100 , Mousey  - 100);
+            }
             
-            if(mousereleased1 && Fuoco.getX() - Acqua.getX()<200){
+            if(mousereleasedFuoco && Fuoco.getX() - Acqua.getX()>=10 && Fuoco.getY() - Acqua.getY()>=10){
+                
                 /*Vento=new JLabel("");
                 Vento.setSize(200, 200);
                 Vento.setLocation(700,400);
                 Vento.setIcon(new ImageIcon("vento.png"));
-                Vento.addMouseListener(ml4);
+                Vento.addMouseListener(mlVento);
                 this.add(Vento);*/
                 
                 Terra=new JLabel("");
                 Terra.setSize(200, 200);
-                Terra.setLocation(500,300);
+                Terra.setLocation(960,540);
                 Terra.setIcon(new ImageIcon("Terra.png"));
-                Terra.addMouseListener(ml3);
+                Terra.addMouseListener(mlTerra);
                 this.add(Terra);
-                Fuoco.setLocation(1000,500);
-                Acqua.setLocation(200,500);
+                Fuoco.setLocation(1700,50);
+                Acqua.setLocation(1700,350);
                 
             }
+            
+            if(mousereleasedFuoco && Fuoco1.getX() - Fuoco.getX()>=10 && Fuoco1.getY() - Fuoco.getY()>=10){
+                
+                Energia=new JLabel("");
+                Energia.setSize(200, 200);
+                Energia.setLocation(960,540);
+                Energia.setIcon(new ImageIcon("energia.png"));
+                Energia.addMouseListener(mlEnergia);
+                this.add(Energia);
+                Fuoco.setLocation(1700,50);
+                Fuoco1.setLocation(1000,50);
+                
+            }
+            
+            
         }
     }
     
-    
-    
-    MouseListener ml1 = new MouseListener(){
+    MouseListener mlFuoco = new MouseListener(){
         @Override
         public void mouseClicked(MouseEvent me) {}
         @Override
         public void mousePressed(MouseEvent me) {
-            mousepressed1=true;
-            mousereleased1=false;
+            mousepressedFuoco=true;
+            mousereleasedFuoco=false;
         }
         @Override
         public void mouseReleased(MouseEvent me) {
-            mousepressed1=false;
-            mousereleased1=true;
+            mousepressedFuoco=false;
+            mousereleasedFuoco=true;
         }
         @Override
         public void mouseEntered(MouseEvent me) {}
@@ -119,20 +164,18 @@ public class GiocoChimica extends JFrame {
         public void mouseExited(MouseEvent me) {}  
     };
     
-    
-    
-    MouseListener ml2 = new MouseListener(){
+    MouseListener mlFuoco1 = new MouseListener(){
         @Override
         public void mouseClicked(MouseEvent me) {}
         @Override
         public void mousePressed(MouseEvent me) {
-            mousepressed2=true;
-            mousereleased2=false;
+            mousepressedFuoco1=true;
+            mousereleasedFuoco1=false;
         }
         @Override
         public void mouseReleased(MouseEvent me) {
-            mousepressed2=false;
-            mousereleased2=true;
+            mousepressedFuoco1=false;
+            mousereleasedFuoco1=true;
         }
         @Override
         public void mouseEntered(MouseEvent me) {}
@@ -140,18 +183,18 @@ public class GiocoChimica extends JFrame {
         public void mouseExited(MouseEvent me) {}  
     };
     
-    MouseListener ml3 = new MouseListener(){
+    MouseListener mlAcqua = new MouseListener(){
         @Override
         public void mouseClicked(MouseEvent me) {}
         @Override
         public void mousePressed(MouseEvent me) {
-            mousepressed3=true;
-            mousereleased3=false;
+            mousepressedAcqua=true;
+            mousereleasedAcqua=false;
         }
         @Override
         public void mouseReleased(MouseEvent me) {
-            mousepressed3=false;
-            mousereleased3=true;
+            mousepressedAcqua=false;
+            mousereleasedAcqua=true;
         }
         @Override
         public void mouseEntered(MouseEvent me) {}
@@ -159,18 +202,56 @@ public class GiocoChimica extends JFrame {
         public void mouseExited(MouseEvent me) {}  
     };
     
-    MouseListener ml4 = new MouseListener(){
+    MouseListener mlTerra = new MouseListener(){
         @Override
         public void mouseClicked(MouseEvent me) {}
         @Override
         public void mousePressed(MouseEvent me) {
-            mousepressed4=true;
-            mousereleased4=false;
+            mousepressedTerra=true;
+            mousereleasedTerra=false;
         }
         @Override
         public void mouseReleased(MouseEvent me) {
-            mousepressed4=false;
-            mousereleased4=true;
+            mousepressedTerra=false;
+            mousereleasedTerra=true;
+        }
+        @Override
+        public void mouseEntered(MouseEvent me) {}
+        @Override
+        public void mouseExited(MouseEvent me) {}  
+    };
+    
+    MouseListener mlVento = new MouseListener(){
+        @Override
+        public void mouseClicked(MouseEvent me) {}
+        @Override
+        public void mousePressed(MouseEvent me) {
+            mousepressedVento=true;
+            mousereleasedVento=false;
+        }
+        @Override
+        public void mouseReleased(MouseEvent me) {
+            mousepressedVento=false;
+            mousereleasedVento=true;
+        }
+        @Override
+        public void mouseEntered(MouseEvent me) {}
+        @Override
+        public void mouseExited(MouseEvent me) {}  
+    };
+    
+    MouseListener mlEnergia = new MouseListener(){
+        @Override
+        public void mouseClicked(MouseEvent me) {}
+        @Override
+        public void mousePressed(MouseEvent me) {
+            mousepressedEnergia=true;
+            mousereleasedEnergia=false;
+        }
+        @Override
+        public void mouseReleased(MouseEvent me) {
+            mousepressedEnergia=false;
+            mousereleasedEnergia=true;
         }
         @Override
         public void mouseEntered(MouseEvent me) {}
